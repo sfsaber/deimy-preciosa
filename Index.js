e1858ea6fe5725e61afcd24d3b4f5ce28b7642d0
sfsaber <54611010+sfsaber@users.noreply.github.com>
2019-08-28 08:20:16 -0500

    commit

---
+++ Index.js
@@ -0,0 +1,51 @@
+var respuestasC = [0, 0, 0, 0, 1, 1, 0, 0, 1]
+var RespuestasUser = new Array(9)
+var color = new Array(3)
+var count = 0
+
+
+$(document).ready(function () {
+
+      
+    
+});
+
+function revisar(){
+    for (let index = 0; index < respuestasC.length; index++) {
+        RespuestasUser[index] = $('input[name="' + (index + 1) + '"]:checked').attr("value");
+    }
+    const swalWithBootstrapButtons = Swal.mixin({
+        customClass: {
+          confirmButton: 'btn btn-success',
+          cancelButton: 'btn btn-danger'
+        },
+        buttonsStyling: false
+      })
+      
+      swalWithBootstrapButtons.fire({
+        title: 'Quieres ser la novia de Carlitos? 👰',
+        text: "Si aceptas, pasaremos el mejor año de nuestras vidas juntos y viviremos experiencias muy bonitas que recordaremos por siempre",
+        type: 'warning',
+        showCancelButton: true,
+        cancelButtonText: 'No, quiero solo setso 💔',
+        confirmButtonText: 'Sí, quiero pasar el mejor año de mi vida con Carlitos como su novia! 💖',
+        reverseButtons: true
+      }).then((result) => {
+        if (result.value) {
+          swalWithBootstrapButtons.fire(
+            'Gracias por aceptar ser mi novia preciosa',
+            'Te quiero muchisimo y me haces muy feliz flaquita 💕',
+            'success'
+          )
+        } else if (
+     
+          result.dismiss === Swal.DismissReason.cancel
+        ) {
+          swalWithBootstrapButtons.fire(
+            'Pos bueno',
+            'Al menos no intenté 💔',
+            'error'
+          )
+        }
+      })
+}
